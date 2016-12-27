bed75d9b52ccdf8e278bf2b08858718d3ae0aebe
Yaroslav Liakh <liakh_ya@mail.ru>
2016-12-27 19:02:17 +0200

    ready task

--- arr_of_time.rb
+++ arr_of_time.rb
@@ -1,18 +1,19 @@
-def arr_time(date_string)
+def arr_time(date_string) # функція, що повертає масив часу від заданої дати до 5 днів
 
-  def do_function(plus_time, counter, variable)
+  def do_function(plus_time, counter, variable) # функція обробки дат
     (0..counter).each do
       variable=variable.to_i + plus_time
       @transform_at=Time.at(variable)
-      @normalise=@transform_at.strftime('%a, %d %b %Y %H:%M:%S')
+      @normalise=@transform_at.strftime('%a, %d %b %Y %H:%M:%S') #нормалізація дат, до зрозумілого людині вигляду
       @res_arr.push(@normalise.to_s)
     end
-    variable
+    variable # повернення останньої обробленої дати, потрібно, щоб наступний період часу знав, з якого часу почати відлік
   end
 
   @res_arr=Array.new
+  # хеш місяців, потрібен для визначення номеру місяця за порядком
   months={'Jan' => 1, 'Feb' => 2, 'Mar' => 3, 'Apr' => 4, 'May' => 5, 'June' => 6, 'July' => 7, 'Aug' => 8, 'Sept' => 9, 'Oct' => 10, 'Nov' => 11, 'Dec' => 12}
-
+  # блок розбиття початкової дати на елементи масиву, комфортні для обробки
   date_string=date_string.split(' ')
   time= date_string[4].to_s.split(':')
 
@@ -20,8 +21,9 @@ def arr_time(date_string)
     if date_string[2].to_s==mounth
       @mounth=months[mounth]
     end }
-
+  # формування першого елементу масиву кінцевих даних
   @unix_time=Time.local(date_string[3].to_i, @mounth, date_string[1].to_i, time[0].to_i, time[1].to_i, time[2].to_i)
+  # виклик функцій обробки дат
   do_function(86400, 4, do_function(3600, 6, do_function(1800, 7, do_function(900, 3, do_function(0, 0, @unix_time)))))
 
   @res_arr
--- diffTime.rb
+++ diffTime.rb
@@ -1,8 +1,8 @@
-def diff_time(f_time, s_time)
-
+def diff_time(f_time, s_time) #функція обробки дат заданих в unix-форматі
+  # блок приведення даних до нормальної їх обробки
   f_time = f_time.to_i.abs
   s_time = s_time.to_i.abs
-
+  # визначення, яка з дат більша, для знаходження різниці між ними в unix-форматі
   if f_time == s_time
     return 'the same date'
   elsif f_time >= s_time
@@ -14,14 +14,14 @@ def diff_time(f_time, s_time)
   end
 
   @time_difference_in_unix = @max_time - @min_time
-
+  # визначення різниці між датами в зрозумілому, для людини, форматі
   @day=@time_difference_in_unix/(60*60*24)
   @hour=(@time_difference_in_unix - @day*24*60*60)/(60*60)
   @minutes=(@time_difference_in_unix - @day*24*60*60 - @hour*60*60)/60
   @seconds=@time_difference_in_unix - @day*24*60*60 - @hour*60*60 - @minutes*60
-
+  # блок функцій нормалізації вигляду з правильними закінченнями
   private
-  def sec(sec)
+  def sec(sec) #функція нормалізації вигляду секунд
     if sec==0
       sec=''
     elsif (@day==''||@day==0)&&(@hour==''||@hour==0)&&(@minutes==' '||@minutes==0)
@@ -31,7 +31,7 @@ def diff_time(f_time, s_time)
     end
   end
 
-  def normalize_time(time_val)
+  def normalize_time(time_val) #функція нормалізації вигляду днів та годин
     if time_val.to_i>1
       time_val=time_val.to_s + 's'
     elsif time_val.to_i==0
@@ -41,18 +41,19 @@ def diff_time(f_time, s_time)
     end
   end
 
-  def normalize_min(minutes)
+  def normalize_min(minutes) #функція нормалізації вигляду хвилин
     if minutes.to_i>=1
       minutes=minutes.to_s + ' min'
     else
       minutes=''
     end
   end
-
+ #  повертає результат роботи функції
  normalize_time(@day.to_s + ' day') + ' ' +
      normalize_time(@hour.to_s + ' hour') + ' ' +
      normalize_min(@minutes.to_s) +
      sec(@seconds)
 end
 
+#  блок вхідних даних
 puts diff_time(1478779622, '1478779621')
